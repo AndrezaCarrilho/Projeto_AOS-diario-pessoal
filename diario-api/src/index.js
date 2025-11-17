f859143a58159dd7d02898b597f896e64f40a261
AndrezaCarrilho <108102040+AndrezaCarrilho@users.noreply.github.com>
2025-11-17 21:22:47 +0000

    adiçao pastas

---
+++ diario-api/src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API do Diário Pessoal está no ar!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
