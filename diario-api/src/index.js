7b9a784fafe3b162061d3ca26022c274d22226ab
Natan Soares <natansoares.dev@gmail.com>
2025-12-04 10:56:17 +0000

    UPDATE: Index atualizado com todas as rotas

--- diario-api/src/index.js
+++ diario-api/src/index.js
@@ -4,16 +4,16 @@ const app = express();
 
 app.use(express.json());
 
-// --- 1. IMPORTAÇÕES ---
+//IMPORTAÇÕES
 const authRoutes = require('./routes/authRoutes');
 const cadernoRoutes = require('./routes/cadernoRoutes');
-//const humorRoutes = require('./routes/humorRoutes');
+const humorRoutes = require('./routes/humorRoutes');
 const entradaRoutes = require('./routes/entradaRoutes');
 
-// --- 2. USO DAS ROTAS ---
+//USO DAS ROTAS
 app.use('/auth', authRoutes);       
 app.use('/cadernos', cadernoRoutes); 
-//app.use('/humores', humorRoutes);    
+app.use('/humores', humorRoutes);    
 app.use('/entradas', entradaRoutes);  
 
 // Rota da Página Inicial
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
   res.send('API Diário Pessoal rodando com Sucesso!');
 });
 
-// --- 3. INICIAR SERVIDOR ---
+//INICIAR SERVIDOR
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
